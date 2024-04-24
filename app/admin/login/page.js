fd45e2f0883e6510d87708f047948586725cdf1f
saffiullahfahim <saffiullah.fahim@gmail.com>
2024-04-24 23:09:27 +0600

    refactor: Update login pages to use LoginWrapper component for improved code organization and reusability

--- app/admin/login/page.js
+++ app/admin/login/page.js
@@ -1,32 +1,5 @@
-import LoginForm from "@/components/LoginForm";
-import Link from "next/link";
+import LoginWrapper from "@/components/LoginWrapper";
 
 export default function Login() {
-  return (
-    <>
-      <section id="wrapper">
-        <section className="common-sec login-page-sec">
-          <div className="container">
-            <div className="logo-dv text-center">
-              <Link className="navbar-brand" href="/admin">
-                <span className="site-logo">
-                  <img src="/asset/img/logo.svg" alt="Logo" />
-                </span>
-              </Link>
-            </div>
-            <div className="login-form-dv">
-              <section className="custom-form-sec">
-                <LoginForm type="admin" />
-              </section>
-              {/* custom-form-sec */}
-            </div>
-            {/* login-form-dv */}
-          </div>
-          {/* container */}
-        </section>
-        {/* common-sec */}
-      </section>
-      {/* wrapper */}
-    </>
-  );
+  return <LoginWrapper type="admin" />;
 }
---
+++ components/LoginWrapper.js
@@ -0,0 +1,32 @@
+import LoginForm from "@/components/LoginForm";
+import Link from "next/link";
+
+export default function LoginWrapper({ type }) {
+  return (
+    <>
+      <section id="wrapper">
+        <section className="common-sec login-page-sec">
+          <div className="container">
+            <div className="logo-dv text-center">
+              <Link className="navbar-brand" href="/admin">
+                <span className="site-logo">
+                  <img src="/asset/img/logo.svg" alt="Logo" />
+                </span>
+              </Link>
+            </div>
+            <div className="login-form-dv">
+              <section className="custom-form-sec">
+                <LoginForm type={type} />
+              </section>
+              {/* custom-form-sec */}
+            </div>
+            {/* login-form-dv */}
+          </div>
+          {/* container */}
+        </section>
+        {/* common-sec */}
+      </section>
+      {/* wrapper */}
+    </>
+  );
+}
