9a256c2404e21ef63e03bf9911f070a84ddc5720
saffiullahfahim <saffiullah.fahim@gmail.com>
2024-05-08 15:07:38 +0600

    add report

--- app/admin/report/page.js
+++ app/admin/report/page.js
@@ -1,123 +1,157 @@
-import Link from "next/link";
+"use client";
+
+import Header from "@/components/admin/Header";
+import gasFetch from "@/gasFetch";
+import { useState } from "react";
 
 export default function Report() {
+  const [state, setState] = useState({
+    startDate: "",
+    endDate: "",
+    isLoading: false,
+    error: "",
+    success: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setState((state) => {
+      return { ...state, isLoading: true, error: "", success: "" };
+    });
+
+    try {
+      if (!state.startDate) {
+        throw new Error("Start Date is required!");
+      }
+
+      if (!state.endDate) {
+        throw new Error("End Date is required!");
+      }
+
+      if (
+        !/^(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])\-\d{4}$/.test(
+          state.startDate
+        )
+      ) {
+        throw new Error("Invalid Start Date. It should be MM-DD-YYYY");
+      }
+
+      if (
+        !/^(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])\-\d{4}$/.test(
+          state.endDate
+        )
+      ) {
+        throw new Error("Invalid End Date. It should be MM-DD-YYYY");
+      }
+
+      if (new Date(state.startDate) > new Date(state.endDate)) {
+        throw new Error("Start Date must be less then or equal End Date.");
+      }
+
+      let response = await gasFetch("/admin/export-report", {
+        startDate: state.startDate,
+        endDate: state.endDate,
+      });
+
+      let responseJSON = await response.json();
+
+      if (responseJSON.status) {
+        // Create a blob from the data csv
+        let blob = new Blob([responseJSON.data], {
+          type: "text/csv",
+        });
+
+        let url = window.URL.createObjectURL(blob);
+        let a = document.createElement("a");
+        a.href = url;
+        a.download = `report_${state.startDate}_${state.endDate}.csv`;
+        a.click();
+
+        setState((state) => {
+          return {
+            ...state,
+            isLoading: false,
+            success: "Report exported successfully",
+            startDate: "",
+            endDate: "",
+          };
+        });
+      } else {
+        setState((state) => {
+          return {
+            ...state,
+            isLoading: false,
+            error: responseJSON.error,
+          };
+        });
+      }
+    } catch (err) {
+      setState((state) => {
+        return {
+          ...state,
+          isLoading: false,
+          error: err.message ?? err,
+        };
+      });
+    }
+  };
+
   return (
     <section id="wrapper">
-      <header className="site-header">
-        <div className="container-fluid">
-          <nav className="navbar site-navigation">
-            <div className="navbar-brand">
-              <Link href="/admin">
-                <img src="/asset/img/logo.svg" alt="Logo" />
-              </Link>
-            </div>
-            <div className="search-dv">
-              <form action="" id="search_form">
-                <button type="submit">
-                  <img src="/asset/img/search-icon.png" alt="Search" />
-                </button>
-                <input
-                  type="text"
-                  name="search"
-                  id="search"
-                  placeholder="Search"
-                />
-              </form>
-              <span className="ic-dv arrow-ic">
-                <a href="#">
-                  <img src="/asset/img/up-dwn-arr.png" alt="Icon" />
-                </a>
-              </span>
-            </div>
-            <ul className="navbar-nav">
-              <li>
-                <Link href="/admin">
-                  <span className="txt">Home</span>
-                </Link>
-              </li>
-              <li>
-                <Link href="/admin/report" className="active">
-                  <span className="icon">
-                    <img
-                      src="/asset/img/report.png"
-                      alt="Report"
-                      className="iconBlack"
-                    />
-                    <img
-                      src="/asset/img/report-white.png"
-                      alt="Report"
-                      className="iconBlue"
-                    />
-                  </span>
-                  <span className="txt">Report</span>
-                </Link>
-              </li>
-              <li>
-                <Link href="/admin/history">
-                  <span className="icon">
-                    <img
-                      src="/asset/img/share-clock.png"
-                      alt="History"
-                      className="iconBlack"
-                    />
-                    <img
-                      src="/asset/img/share-clock-blue.png"
-                      alt="History"
-                      className="iconBlue"
-                    />
-                  </span>
-                  <span className="txt">History</span>
-                </Link>
-              </li>
-              <li>
-                <Link href="/admin/logout">
-                  <span className="icon">
-                    <img src="/asset/img/logout.png" alt="LogOut" />
-                  </span>
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        {/* container */}
-      </header>
+      <Header />
 
       <main className="site-main">
         <section id="wrapper">
           <section className="common-sec login-page-sec">
             <div className="container">
-              <div className="analysis-wrapp mx-auto">
+              <form onSubmit={handleSubmit} className="analysis-wrapp mx-auto">
                 <div className="mdl-input-bx">
-                  <label>Date 1</label>
+                  <label>Start Date</label>
                   <input
                     type="text"
-                    name=""
-                    id=""
+                    value={state.startDate}
+                    onChange={(e) =>
+                      setState({ ...state, startDate: e.target.value })
+                    }
                     className="form-control"
                     autoComplete="off"
-                    required=""
-                    placeholder="Enter Date"
+                    required
+                    disabled={state.isLoading}
+                    placeholder="Enter Start Date"
                   />
                 </div>
                 <div className="mdl-input-bx">
-                  <label>Date 2</label>
+                  <label>End Date</label>
                   <input
                     type="text"
-                    name=""
-                    id=""
+                    value={state.endDate}
+                    onChange={(e) =>
+                      setState({ ...state, endDate: e.target.value })
+                    }
                     className="form-control"
                     autoComplete="off"
-                    required=""
-                    placeholder="Enter Date"
+                    required
+                    disabled={state.isLoading}
+                    placeholder="Enter End Date"
                   />
                 </div>
                 <button
                   type="submit"
+                  disabled={state.isLoading}
                   className="custom-btn w-100 text-uppercase"
                 >
-                  Send
+                  {state.isLoading ? "Loading..." : "Export Report"}
                 </button>
-              </div>
+                {state.error && (
+                  <div className="alert alert-danger mt-3">{state.error}</div>
+                )}
+                {state.success && (
+                  <div className="alert alert-success mt-3">
+                    {state.success}
+                  </div>
+                )}
+              </form>
               {/* analysis-wrapp */}
             </div>
             {/* container */}
